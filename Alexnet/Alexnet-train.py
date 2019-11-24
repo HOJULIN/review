ea71ee19718caaf75aed404636cd61b3a030db59
HOJULIN <2509042973@qq.com>
2019-11-24 16:05:22 +0800

    k

---
+++ Alexnet/Alexnet-train.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#tensorflow 2.0
+# @Time    : 2019/11/24 13:25
+# @File    : Alexbet-train.py
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import models
+from tensorflow.python.keras import layers
+import  matplotlib.pyplot as plt
+print(tf.__version__)
+
+mnist=tf.keras.datasets.mnist
+import Alexnet
+
+
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+#x_train的形状为(60000, 28, 28)，y_train的形状为(60000,),x_test的形状为(10000, 28, 28)
+
+x_train=x_train.reshape((-1,28,28,1))
+#此型状为(60000, 28, 28, 1) 60000是数量，1代表是单色图像
+x_test=x_test.reshape((-1,28,28,1))
+
+#print(x_train.shape)
+print(x_train.shape[0],x_train.shape[1])
+x_shape=x_train.shape
+
+def AlexNet_train():
+    AlexNet_model=Alexnet.AlexNet_inference(x_shape)
+    totall_epochs=0
+    epochs=10
+
+    while(True):
+        history=AlexNet_model.fit(x_train,y_train,batch_size=60,epochs=epochs,validation_split=0.1)
+
+        #fit函数返回一个History的对象，其History.history属性记录了损失函数和其他指标的数值随epoch变化的情况
+        plt.plot(history.history['accuracy'])
+        plt.legend(['training','valivation'],loc='upper left')
+        plt.show()
+
+        res=AlexNet_model.evaluate(x_test,y_test)
+        print(res)
+
+        totall_epochs+=epochs
+        model_save_dir='AlexNet_model_'+str(totall_epochs)+".h5"
+        AlexNet_model.save(model_save_dir)
+
+        keyVal=input("0:quit,1:continue")
+        keyVal=int(keyVal)
+        if 0==keyVal:
+            break
+        else:
+            epochs=keyVal
+
+
+AlexNet_train()
+
+
---
+++ Alexnet/Alexnet.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#tensorflow 2.0
+# @Time    : 2019/11/23 20:57
+# @File    : Alexnet.py
+#  结构如图https://www.jianshu.com/p/00a53eb5f4b3
+import tensorflow as tf
+from tensorflow.python.keras import models
+from tensorflow.python.keras import layers
+
+#注意要加上python
+
+def AlexNet_inference(in_shape):    #输入图片的形状
+    model=models.Sequential(name="AlexNet")
+    model.add(layers.Conv2D(96,(11,11),strides=(2,2),input_shape=(in_shape[1],in_shape[2],in_shape[3]),
+                     padding='same',activation='relu'))
+    #kernel_initializer='uniform'这个是用来神魔？
+    model.add(layers.MaxPooling2D( pool_size=(3, 3), strides=(2, 2)))
+    model.add(layers.Conv2D(256,(5,5),strides=(1,1),
+                     padding='same',activation='relu'))
+    model.add(layers.MaxPooling2D( pool_size=(3, 3), strides=(2, 2)))
+    model.add(layers.Conv2D(384,(3,3),strides=(1,1),
+                     padding='same',activation='relu'))
+    model.add(layers.Conv2D(384, (3, 3), strides=(1, 1),
+                     padding='same', activation='relu' ))
+    model.add(layers.Conv2D(256, (3,3), strides=(1, 1),
+                            padding='same', activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(2048,activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(2048,activation='relu'))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(10, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',  # 不能直接用函数，否则在与测试加载模型不成功！
+                  metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+
+
+
+
+
